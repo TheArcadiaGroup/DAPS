71e42a90fc3067243dd03da64c93cb78440ef5d8
lyricidal <lyricidal@gmail.com>
2019-09-20 11:49:13 -0400

    Update Mac deploy related files

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -1,6 +1,5 @@
 // Copyright (c) 2011-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
-// Copyright (c) 2015-2018 The PIVX developers
 // Copyright (c) 2018-2019 The DAPScoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -50,8 +49,8 @@ static const int MAX_URI_LENGTH = 255;
 #define SPINNER_FRAMES 35
 
 #define QAPP_ORG_NAME "DAPScoin"
-#define QAPP_ORG_DOMAIN "dapscoin.org"
-#define QAPP_APP_NAME_DEFAULT "DAPScoin-Qt"
+#define QAPP_ORG_DOMAIN "dapscoin.com"
+#define QAPP_APP_NAME_DEFAULT "DAPScoin-Qt-Multisig"
 #define QAPP_APP_NAME_TESTNET "DAPScoin-Qt-testnet"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H
