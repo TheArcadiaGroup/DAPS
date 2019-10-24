ac26cc9ae115fceef432885873150ecacd9c5ede
lyricidal <lyricidal@gmail.com>
2019-10-24 15:12:42 -0400

    fix wallet crash when selecting decoys
    https://github.com/ArcadiaMediaGroup/DAPS/commit/758f97643d89256c5ebbad5f46d5c32d50fae3b8
    
    (Note: these are Develop block heights)

--- src/pow.cpp
+++ src/pow.cpp
@@ -297,7 +297,7 @@ bool CheckNumberOfAuditedPoSBlocks(const CBlock& block)
 bool CheckPoABlockMinedHash(const CBlockHeader& block)
 {
     const uint256 minedHash = block.minedHash; //block.ComputeMinedHash();
-    if (minedHash == block.minedHash && block.nBits == GetNextWorkRequired(mapBlockIndex[block.hashPrevBlock], &block)) {
+    if (minedHash == block.minedHash) {
         //Check minedHash satisfy difficulty based on nbits
         bool fNegative;
         bool fOverflow;
