85340bf08d0b7d90ddb305b90d3e1df29ea46487
zeus <dream0329@outlook.com>
2019-11-06 15:58:08 -0800

    DAPS-1151 rollback

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -70,8 +70,7 @@ public:
 #endif // ENABLE_WALLET
     bool enableWallet;
     bool fMultiSend = false;
-    RPCConsole* rpcConsole;
-
+    
 protected:
     void changeEvent(QEvent* e);
     void closeEvent(QCloseEvent* event);
@@ -127,12 +126,13 @@ private:
     QFrame* frameBlocks;
     QLabel *stakingState;
     QLabel* connectionCount;
-
+    
     QProgressDialog* progressDialog = nullptr;
 
     QSystemTrayIcon* trayIcon;
     QMenu* trayIconMenu;
     Notificator* notificator;
+    RPCConsole* rpcConsole;
     BlockExplorer* explorerWindow;
     bool showTooltips = false;
 
